0386fe6a286e44b57b3d80aaa74f2baee49a6e88
Tobias Kohn <webmaster@tobiaskohn.ch>
2018-09-12 11:18:09 +0100

    Changed name

--- examples/run_example.py
+++ examples/run_example.py
@@ -2,11 +2,11 @@
 # (c) 2018, Tobias Kohn
 #
 # Created: 23.08.2018
-# Updated: 11.09.2018
+# Updated: 12.09.2018
 #
 # License: Apache 2.0
 #
-from pypat import enable_auto_import
+from pmatch import enable_auto_import
 
 ##############################################################
 #                                                            #
